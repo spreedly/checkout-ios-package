e6be6fdf7a88216f1a04e2071462e6a0d30f6e50
Sandesh Jain <sajain@spreedly.com>
2025-11-07 12:59:08 +0530

    Update Package.swift (#52)

--- Package.swift
+++ Package.swift
@@ -34,11 +34,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "SpreedlyCore", 
-            path: "./Frameworks/SpreedlyCore.xcframework",
-            dependencies: [
-                .product(name: "DatadogCore", package: "dd-sdk-ios"),
-                .product(name: "DatadogLogs", package: "dd-sdk-ios")
-            ]
+            path: "./Frameworks/SpreedlyCore.xcframework"
         ),
         .binaryTarget(name: "SpreedlySecurity", path: "./Frameworks/SpreedlySecurity.xcframework"),
         .binaryTarget(name: "SpreedlyUI", path: "./Frameworks/SpreedlyUI.xcframework"),
